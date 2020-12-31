5d532512aa33bcb9b3bcf7359abfe66ed8999f24
junghun yang <luciferkala@gmail.com>
2020-12-31 12:30:07 +0900

    add controller

---
+++ src/controllers/services/board/FindOneBoardController.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from "express";
+
+import Controller from "@src/controllers/Controller";
+
+import BoardService from "@src/services/BoardService";
+
+import resTypes from "@src/utils/resTypes";
+import Board from "@src/models/BoardModel";
+
+class CreateBoardController extends Controller {
+    private result: Board | string;
+    constructor() {
+        super();
+        this.result = "";
+    }
+
+    async doService(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            this.result = await BoardService.findOne(req);
+        } catch (e: unknown) {
+            this.result = "InternalServerError";
+            console.log(e);
+        }
+    }
+
+    async doResolve(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        switch (this.result) {
+            case "BadRequest":
+                resTypes.badRequestErrorRes(res);
+                break;
+            case "InternalServerError":
+                resTypes.internalErrorRes(res);
+                break;
+            case "CannotFindItem":
+                resTypes.cannotFindItemRes(res, "user");
+                break;
+            default:
+                resTypes.successRes(res, "특정 게시판 조회");
+        }
+    }
+}
+
+export default CreateBoardController;
---
+++ src/controllers/services/post/FindOneEnvDataController.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from "express";
+
+import Controller from "@src/controllers/Controller";
+
+import PostService from "@src/services/PostService";
+
+import resTypes from "@src/utils/resTypes";
+import Post from "@src/models/PostModel";
+
+class SigninController extends Controller {
+    private result: Post | string;
+    constructor() {
+        super();
+        this.result = "";
+    }
+
+    async doService(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            this.result = await PostService.findOne(req);
+        } catch (e: unknown) {
+            this.result = "InternalServerError";
+            console.log(e);
+        }
+    }
+
+    async doResolve(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        switch (this.result) {
+            case "BadRequest":
+                resTypes.badRequestErrorRes(res);
+                break;
+            case "InternalServerError":
+                resTypes.internalErrorRes(res);
+                break;
+            case "CannotFindItem":
+                resTypes.cannotFindItemRes(res, "user");
+                break;
+            default:
+                resTypes.successRes(res, "특정 게시물 조회");
+        }
+    }
+}
+
+export default SigninController;
